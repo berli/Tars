3a62f5b26513c640bb441c5d2c189499cb541318
berli <libo22@gmail.com>
2018-08-27 16:58:08 +0800

    new file:   examples/QuickStartDemo/HelloServer/Log.h 	new file:   examples/QuickStartDemo/HelloServer/server.config.conf

---
+++ cpp/examples/QuickStartDemo/HelloServer/Log.h
@@ -0,0 +1,12 @@
+#ifndef __LOG_MACRO_H__
+#define __LOG_MACRO_H__
+
+#include "servant/TarsLogger.h"
+using namespace tars;
+
+#define LOG_INFO		(LOG->info() << __FILE__ << ":" << __LINE__ << "[" << __FUNCTION__ << "]")
+#define LOG_DEBUG		(LOG->debug() << __FILE__ << ":" << __LINE__ << "[" << __FUNCTION__ << "]")
+#define LOG_WARN		(LOG->warn() << __FILE__ << ":" << __LINE__ << "[" << __FUNCTION__ << "]")
+#define LOG_ERROR		(LOG->error() << __FILE__ << ":" << __LINE__ << "[" << __FUNCTION__ << "]")
+
+#endif
